57cbcb781ed9211cabacefce6c2e5371f8b16549
Lotpite <exstornado@gmail.com>
2023-04-12 09:33:22 +0300

    added vercel jsn

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,11 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const DB_USERNAME = process.env.DB_USERNAME || "";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
-const DB_URL =
-  `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@gymapp.7iu81mg.mongodb.net/gympit` ||
-  "";
+const DB_URL = process.env.DB_URL || "";
 const SERVER_PORT = process.env.SERVER_PORT || 4444;
 
 export const config = {
