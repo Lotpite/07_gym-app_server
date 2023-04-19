5685082a5fc9cfa010d6ea183f0c6f403d710225
Lotpite <exstornado@gmail.com>
2023-04-19 09:46:32 +0300

    cors #2

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import router from "../routes/user.router";
 const app = express();
 app.use(
   cors({
-    origin: "https://gympit-b.vercel.app/",
+    origin: "https://gympit-b.vercel.app",
   })
 );
 const jsonBodyMiddleware = express.json();
